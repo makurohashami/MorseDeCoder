cf214f0001661cf86669492066dab43607ab1826
Y'Kotyk <makurohashami@gmail.com>
2023-02-15 15:15:03 +0200

    Test in Main class

--- src/main/java/com/kotyk/morsedecoder/Main.java
+++ src/main/java/com/kotyk/morsedecoder/Main.java
@@ -1,7 +1,15 @@
 package com.kotyk.morsedecoder;
 
+import com.kotyk.morsedecoder.parser.MorseDeCoder;
+import com.kotyk.morsedecoder.parser.MorseDeCoderImpl;
+
 public class Main {
     public static void main(String[] args) {
-        
+        MorseDeCoder deCoder = new MorseDeCoderImpl();
+
+        System.out.println(deCoder.parseToMorse("Factory Method is a...? !@.,"));
+        System.out.println(deCoder.parseToText(deCoder.parseToMorse("Factory Method is ah? !@.,")));
+        System.out.println(deCoder.parseToText(deCoder.parseToMorse("Imagine that you’re creating a logistics management application.")));
+
     }
 }
